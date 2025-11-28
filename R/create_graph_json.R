bbd2b88020b9c4fdfe709fe5b80c7f5eceaeb26b
Josep Pueyo-Ros <jpueyo@icra.cat>
2025-11-28 10:16:54 +0100

    afegeix cabals ambientals

--- R/create_graph_json.R
+++ R/create_graph_json.R
@@ -1,92 +1,5 @@
 source("R/read_db.R")
-library(tidyverse)
-use('janitor', 'clean_names')
-library(sf)
-library(lwgeom)
-library(jsonlite)
-set.seed(4)
-
-nodes <- read_sf("data_raw/nodes_natural_antropic.gpkg") |>
-  select(-c(flow_change, node_id, ma, nearest_node))
-
-nodes <- nodes |>
-  left_join(read_rds("data_raw/conques_dades_cabal.rds"), by = 'codi_sad') |>
-  left_join(read_rds("data_raw/cabals_antropic.rds"), by = 'codi_sad')
-
-stopifnot(
-  nodes |>
-    filter(type == 'massa' | type == 'comporta') |>
-    filter(if_any(area_m2:neu12, \(x) is.na(x))) |>
-    nrow() ==
-    0
-)
-
-stopifnot(
-  nodes |>
-    filter(type != 'massa' & type != 'comporta') |>
-    filter(if_any(m1:m12, \(x) is.na(x))) |>
-    nrow() ==
-    0
-)
-
-masses <- read_sf(
-  "data_raw/MASSES_AIGUA_BE.gpkg",
-  layer = "retall_embassament"
-)
-
-# Comprova que tots els nodes estan sobre els arcs
-stopifnot(all(nodes |> st_intersects(masses, sparse = FALSE) |> rowSums() > 0))
-
-# Divideix les línies segons els nodes, calcula la distància i dona noms correlatius als trams partits
-edges <- st_split(masses, nodes) |>
-  st_collection_extract("LINESTRING") %>%
-  mutate(river_length = st_length(.)) |>
-  mutate(nom = str_remove_all(NOM_COMU, " \\d$"), .before = 1) |>
-  mutate(numero = row_number(), .by = nom, .after = nom) |>
-  mutate(n = n(), .by = nom, .after = numero) |>
-  mutate(
-    nom_correlatiu = if_else(n == 1, nom, paste(nom, numero)),
-    .before = everything()
-  ) |>
-  select(-c(nom, numero, n))
-
-## Per quan tinguem edges amb geom ------------------------------
-
-froms <- st_startpoint(edges) |>
-  st_as_sf() |>
-  st_join(nodes) |>
-  pull(codi_sad)
-
-stopifnot(all(!is.na(froms)))
-
-tos <- st_endpoint(edges) |>
-  st_as_sf() |>
-  st_join(nodes) |>
-  pull(codi_sad)
-
-edges$from <- froms
-edges$to <- tos
-
-# Comprovacions ----------------------------------------------------
-
-if (any(duplicated(edges$from))) {
-  rlang::abort("Hi ha duplicats a from dels arcs")
-}
-
-stopifnot(
-  all(edges$from %in% nodes$codi_sad) && all(edges$to %in% nodes$codi_sad)
-)
-
-# comprova que tots els nodes tenen arcs sortint excepte els finals
-stopifnot(length(which(nodes$codi_sad %in% edges$from)) == (nrow(nodes) - 5))
-stopifnot(
-  nodes$codi_sad[which(!(nodes$codi_sad %in% edges$from))] ==
-    c("NODE_33", "NODE_34", "NODE_63", "NODE_82", "NODE_84")
-)
-
-# Atribuïm valors random ----------------------------------------------------------------
-
-edges$flow_need <- sample(2:10, nrow(edges), replace = T)
+source("R/load_graph.R")
 
 # Guardem la xarxa a assets -----------------------------------------------------
 
@@ -98,7 +11,6 @@ nodes |>
 edges |>
   rename(
     nomComu = nom_correlatiu,
-    flowNeed = flow_need,
     lengthRiver = river_length,
     codiMassa = EUMSPFCOD
   ) |>
--- js/app.js
+++ js/app.js
@@ -6,27 +6,38 @@ const { createApp, onMounted, ref, shallowRef, watch } = Vue
 
 const TT_OPTS = { direction: 'auto', sticky: true, opacity: 0.95, className: 'cytt', offset: [10, 0], pane: 'tipPane' }
 
-const fmt = (v, d=1, convert=true) => {
+const fmt = (v) => {
     let unit = 'm³/s'
+    let d = 2
     if (Math.abs(+v) < 0.1) {
         unit = 'l/s'
         // convertim a litres per segon
         v = +v * 1000
+        d = 0
     }
     return Number.isFinite(+v) ? (+v).toFixed(d) + ' ' + unit : '—'
 }
 
 // HTML dels tooltips
 function nodeTooltipHTML(n, month) {
+    if (n.id() === 'DESEMBASSAT') {
+        return `
+            <div>
+              <div><strong>${n.data('name') ?? n.data('id') ?? ''}</strong></div>
+              <div>Tipus: ${n.data('type') ?? '—'}</div>
+              <div>Cabal desembassat: ${fmt(n.data('outflow' + month))}</div>
+            </div>
+        `
+    }
     return `
-    <div>
-      <div><strong>${n.data('id') ?? ''}</strong></div>
-      <div>Tipus: ${n.data('type') ?? '—'}</div>
-      <div>Cabal entrant: ${fmt(n.data('inflow' + month))}</div>
-      <div>${n.data('m' + month) > 0 ? 'Aportació' : 'Extracció'}: ${fmt(n.data('m' + month), 2)}</div>
-      <div>Cabal sortint: ${fmt(n.data('outflow' + month))}</div>
-      <div>Dèficit: ${fmt(n.data('deficit' + month), 2)}</div>
-    </div>
+        <div>
+          <div><strong>${n.data('name') ?? n.data('id') ?? ''}</strong></div>
+          <div>Tipus: ${n.data('type') ?? '—'}</div>
+          <div>Cabal entrant: ${fmt(n.data('inflow' + month))}</div>
+          <div>${n.data('m' + month) > 0 ? 'Aportació' : 'Extracció'}: ${fmt(n.data('m' + month), 2)}</div>
+          <div>Cabal sortint: ${fmt(n.data('outflow' + month))}</div>
+          <div>Dèficit: ${fmt(n.data('deficit' + month), 2)}</div>
+        </div>
   `
 }
 
@@ -36,7 +47,7 @@ function edgeTooltipHTML(e, month) {
       <div><strong>${e.data('name') ?? ''}</strong></div>
       <div>${e.data('codiMassa')}</div>
       <div>Cabal mitjà: ${fmt(e.data('flow' + month))}</div>
-      <div>Cabal ambiental: ${fmt(e.data('flowNeed'))}</div>
+      <div>Cabal ambiental: ${fmt(e.data('envFlow' + month))}</div>
       <div>Llargada tram: ${+e.data('lengthRiver').toFixed(0)} m</div>
     </div>
   `
@@ -126,18 +137,16 @@ createApp({
                     }
                 })
 
-                const cyEdges = edgesGeo.features.map(f => {
-                    return {
-                        data: {
-                            id: f.properties.id,
-                            source: f.properties.from,
-                            target: f.properties.to,
-                            codiMassa: f.properties.codiMassa,
-                            flowNeed: f.properties.flowNeed,
-                            lengthRiver: f.properties.lengthRiver,
-                            name: f.properties.nomComu
+                const cyEdges = edgesGeo.features.map(e => {
+                    const edgeData = Object.keys(e.properties).reduce((acc, key) => {
+                        acc[key] = e.properties[key];
+                        return acc;
+                    }, {});
+                    edgeData.source = e.properties.from;
+                    edgeData.target = e.properties.to;
 
-                        }
+                    return {
+                        data: edgeData
                     }
                 })
 
@@ -364,7 +373,7 @@ createApp({
                     }
                 }).addTo(map);
 
-                gm.calculateContribution(cy.value)
+                gm.calculateContribution(cy.value, gm.params)
                 gm.calculateFlow(cy.value, errorMsg, {period: {year: 2024, month: 8}}); // mesos de l'1 al 12
                 gm.setGraphColors(month.value, cy.value, {nodeLayerById, edgeLayerById});
                 gm.setupEleClickListener(cy.value, selectedEle)
@@ -408,7 +417,8 @@ createApp({
             reset,
             month,
             monthSelector,
-            loading
+            loading,
+            fmt
         }
     }
 }).mount('#app')
